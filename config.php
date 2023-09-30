93ba29c6cdf9387e776805062ef439fae3ade19c
KhalilAndolsi <114878060+KhalilAndolsi@users.noreply.github.com>
2023-09-30 11:26:03 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,13 +4,13 @@
   header("Access-Control-Allow-Headers: Content-Type"); 
   header('Content-Type: application/json'); 
 
-  $db_host = 'fdb1033.awardspace.net';
-  $db_user = '4373693_mydb';    
-  $db_password = 'FCDdAWqr5!+epCU';   
-  $db_name = '4373693_mydb';  
+  $db_host = 'ep-calm-thunder-96135345-pooler.us-east-1.postgres.vercel-storage.com';
+  $db_user = 'default';    
+  $db_password = '9cZC5PnSkIWX';   
+  $db_name = 'verceldb';  
 
   try {
-    $db = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_password);
+    $db = new PDO("pgsql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_password);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ); // Set default fetch mode to objects
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // Disable emulated prepared statements
   } catch(PDOException $e) {
